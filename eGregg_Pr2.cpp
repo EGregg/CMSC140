0a531eefe528f3dec34f7bd1101f702728daabf8
EGregg <creosino@gmail.com>
2019-10-13 20:09:35 -0400

    fixed cases

--- eGregg_Pr2.cpp
+++ eGregg_Pr2.cpp
@@ -22,35 +22,59 @@ Print your Name here: __________
 */
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
 	int choice;
-	float temperature;
-	int distance;
-	int weight;
+	float temperatureC;
+	float temperatureF;
+	float distanceK;
+	float distanceM;
+	float weightK;
+	float weightP;
+	
 	cout << "Converter Toolkit" << endl;
 	cout << "--------------------" << endl;
 	cout << "\t1. Temperature Converter" << endl;
 	cout << "\t2. Distance Converter" << endl;
 	cout << "\t3. Weight Converter" << endl;
 	cout << "\t4. Quit" << endl;
+	cin >> choice;
 	switch (choice)
 	{
 	case 1: cout << "enter temperature in Celsius" << endl;// code to be executed if n = 1;
-		//convert using F = (9/5)*C + 32
+		cin >> temperatureC; 
+		temperatureF = (1.8 * temperatureC) + 32; //convert using F = (9/5)*C + 32
+		cout << temperatureF << endl; 
 		break;
-	case 2: cout << "enter distance in Kilometers" << endl;// code to be executed if n = 2;
-		//numbers cannot be negative
-		// miles = 0.6*kilometers
-		break;
-	case 3: cout << "enter weight in Kilogram"
-		//numbers cannot be negative
-		//pounds = 2.2*kilograms 
 
+	case 2: cout << "enter distance in Kilometers" << endl;// code to be executed if n = 2;
+		cin >> distanceK; 
+		if (distanceK >= 0) //numbers cannot be negative
+			{
+			distanceM = 0.6 * distanceK; cout << distanceM << endl; // miles = 0.6*kilometers
+			}
+		else
+		{
+			cout << "Distance cannot be Negative" << endl;
+		}
+			break;
+
+	case 3: cout << "enter weight in Kilogram: ";
+		cin >> weightK;
+		if (weightK >= 0) //numbers cannot be negative
+			{
+				weightP = 2.2 * weightK; cout << weightP << endl; //pounds = 2.2*kilograms 
+			}
+		else
+		{
+			cout << "Weight cannot be Negative" << endl;
+		}
+		
 		break;
-	case 4: break;
-	default: // code to be executed if n doesn't match any cases
+
+	case 4: break;// code to be executed if n doesn't match any cases
 	}
 }
